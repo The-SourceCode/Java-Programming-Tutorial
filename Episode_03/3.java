8cdaf63bbb402828928c4e009c560942990b2e19
{TheSourceCode} <The-SourceCode@users.noreply.github.com>
2018-01-17 12:46:54 -0600

    Add files via upload

---
+++ Episode_03/3.java
@@ -0,0 +1,8 @@
+public class Java8 {
+    public static void main(String args[]) {
+        byte lives = 127;
+        short health = 100;
+        int exp = 10000;
+        long levels = 100000;
+    }
+}
